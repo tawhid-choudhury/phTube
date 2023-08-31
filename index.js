50238a028526c32300547e1b729ed8ca13de9c3b
pktatari <tawhidalmuhaimin@gmail.com>
2023-08-31 23:05:08 +0600

    cards thumbnails done

--- index.js
+++ index.js
@@ -6,10 +6,54 @@ const loadTabs = async () => {
   const data = json.data;
 
   for (const i of data) {
-    console.log(i);
+    // console.log(i);
     document.getElementById(
       "tabs"
-    ).innerHTML += `<a id="${i.category_id}" onclick="catagoryClicked('${i.category_id}')" class="tab  bg-gray-300 hover:bg-red-500 hover:text-white font-semibold px-5 rounded-[4px] text-base mx-3">${i.category}</a>`;
+    ).innerHTML += `<a id="${i.category_id}" onclick="catagoryClicked('${i.category_id}')" class="tab  bg-gray-300 hover:bg-red-500 hover:text-white font-semibold px-5 rounded-[4px] text-base">${i.category}</a>`;
+  }
+};
+const loadCards = async (id) => {
+  document.getElementById("cardsContainer").innerHTML = "";
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/videos/category/${id}`
+  );
+  const json = await res.json();
+  const data = json.data;
+  for (const i of data) {
+    console.log(i.authors[0]);
+    document.getElementById("cardsContainer").innerHTML += `
+      <div><!-- card -->
+      <div class="flex justify-center items-center relative">
+      <figure class="max-w-[312px] max-h-[200px]">
+          <img src="${i.thumbnail}" alt="" class="rounded-lg w-[312px] h-[200px]">
+      </figure>
+      <p class="absolute bg-slate-900 text-white px-2 rounded bottom-2 right-2">3hrs 56 min ago</p>
+  </div>
+                <div class="flex gap-3 mt-5 justify-center">
+                    <div class="max-w-[312px] max-h-[200px] flex gap-3">
+                    <div class="w-[70px] h-[50px] ">
+                    <img src="./f7c8a8ca2f16c4cbfd746902cb0f8d57.jpeg" alt=""
+                        class="w-[100%] h-[80%] rounded-[50%]">
+                </div>
+                        <div>
+                            <h3 class="font-bold text-black">Building a Winning UX Strategy Using the Kano Model</h3>
+                            <div class="flex items-center">
+                                <h4 class="py-2">
+                                    Author Name</h4>
+                                <h4 class="text-blue-500 ml-3 text-xl relative">
+                                    <i class="fa-solid fa-certificate"></i>
+                                    <i
+                                        class="fa-solid fa-check absolute text-[10px] text-white top-[11px] right-[6px]"></i>
+                                </h4>
+                            </div>
+                            <h4 class="">
+                                91K views
+                            </h4>
+                        </div>
+                    </div>
+                </div>
+            </div><!-- card end -->
+      `;
   }
 };
 const catagoryClicked = (id) => {
